2787516e39bd74d942373b43e33cb4fe5f3a9ffd
Trevor Blades <tdblades@gmail.com>
2017-10-07 11:41:48 -0700

    Get hot reload working

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,11 @@ import React from 'react';
 const App = () => {
   const text = 'Sorrys';
   const stuff = {red: 'green', color: 'blue'};
-  const {red} = stuff;
-  return <div style={{color: red}}>{text}</div>;
+  const otherstuff = {
+    ...stuff,
+    green: 'red'
+  };
+  return <div style={{color: otherstuff.green}}>{text}</div>;
 };
 
 export default App;
