e7b46e48dc955a80fb99d346ddcb1f95bb06ba42
Trevor Blades <tdblades@gmail.com>
2017-10-07 14:26:43 -0700

    Add keyboard-driven time marking

--- src/components/app.js
+++ src/components/app.js
@@ -3,33 +3,27 @@ import React, {Component} from 'react';
 import PlayBar from './play-bar';
 import carryOn from '../audio/carry-on.mp3';
 
-const words = {
-  0: 'yo',
-  2: 'hi',
-  2.552: 'hello',
-  6.3333: 'statik'
-};
-
-const wordKeys = Object.keys(words)
-  .map(Number)
-  .sort();
-
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.onLoadedData = this.onLoadedData.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
     this.onPause = this.onPause.bind(this);
     this.onPlaying = this.onPlaying.bind(this);
     this.onPlayBarClick = this.onPlayBarClick.bind(this);
     this.playPause = this.playPause.bind(this);
     this.updateTime = this.updateTime.bind(this);
 
+    const bars =
+      'Besides the fact a n***a never been as swank as me I like that Until I shine I sit patiently, so light that';
     this.state = {
       currentTime: 0,
       duration: 0,
       loaded: false,
-      playing: false
+      playing: false,
+      markedWords: {},
+      unmarkedWords: bars.split(' ')
     };
   }
 
@@ -39,6 +33,22 @@ class App extends Component {
       loaded: true,
       duration: event.target.duration
     });
+
+    window.addEventListener('keydown', this.onKeyDown);
+  }
+
+  onKeyDown(event) {
+    // TODO: replace with keycode-js
+    switch (event.keyCode) {
+      case 32:
+        this.playPause();
+        break;
+      case 190:
+        this.markWord();
+        break;
+      default:
+        break;
+    }
   }
 
   onPause() {
@@ -76,23 +86,37 @@ class App extends Component {
     }
   }
 
+  markWord() {
+    this.setState(prevState => ({
+      markedWords: {
+        ...prevState.markedWords,
+        [this.audio.currentTime]: prevState.unmarkedWords[0]
+      },
+      unmarkedWords: prevState.unmarkedWords.slice(1)
+    }));
+  }
+
   renderPlayer() {
     if (!this.state.loaded) {
       return 'Loading...';
     }
 
-    let key = wordKeys[0];
-    for (let i = 0; i < wordKeys.length; i++) {
-      const wordKey = wordKeys[i];
-      if (this.state.currentTime < wordKey) {
-        break;
+    let word;
+    let marks = Object.keys(this.state.markedWords);
+    if (marks.length) {
+      marks = marks.map(Number).sort();
+      for (let i = 0; i < marks.length; i++) {
+        const mark = marks[i];
+        if (this.state.currentTime < mark) {
+          break;
+        }
+        word = this.state.markedWords[mark];
       }
-      key = wordKey;
     }
 
     return (
       <div>
-        <div>{words[key]}</div>
+        <h2>{word || '--'}</h2>
         <a onClick={this.playPause}>{this.state.playing ? 'Pause' : 'Play'}</a>
         <div>
           <PlayBar
@@ -102,6 +126,19 @@ class App extends Component {
         </div>
         <div>{this.state.currentTime}</div>
         <div>{this.state.duration}</div>
+        <h3>Marked</h3>
+        <ul>
+          {marks.map(mark => (
+            <li key={mark}>{`${mark}: ${this.state.markedWords[mark]}`}</li>
+          ))}
+        </ul>
+
+        <h3>Unmarked</h3>
+        <ul>
+          {this.state.unmarkedWords.map((unmarkedWord, index) => (
+            <li key={index.toString()}>{unmarkedWord}</li>
+          ))}
+        </ul>
       </div>
     );
   }
