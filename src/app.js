bb87f6fbfbdce446351a21ebbe47fb0904c256f0
Trevor Blades <tdblades@gmail.com>
2017-10-19 10:14:03 -0700

    Add a drop song

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Format from './pages/format';
 import CarryOn from './songs/carry-on';
+import Drop from './songs/drop';
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact component={Format} path="/" />
       <Route exact component={CarryOn} path="/carry-on" />
+      <Route exact component={Drop} path="/drop" />
     </Switch>
   </Router>
 );
